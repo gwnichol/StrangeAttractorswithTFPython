519841ebf96b2bb940dbecdd620ca6bbe17ad654
gwnichol <gwnichol.m@gmail.com>
2017-02-21 17:28:35 -0600

    Edited the touchlist and analysis

--- annalyisis.py
+++ annalyisis.py
@@ -5,5 +5,19 @@ import numpy as np
 mypath = "./SortTest"
 filenames = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 codes =  []
+results = np.zeros(shape=(25,12))
 for a in filenames:
-    codes.append(a[:12])
+    code = a[:12]
+    codes.append(code)
+    first = 0
+    for letter in code:
+        index = ["A","B","C","D","E",'F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'].index(letter)
+        results[index,first] += 1
+        first += 1
+results = results / ((1/25)*(len(filenames)))
+#print(codes)
+print("After " + str(len(filenames)) + " tests")
+print(results)
+
+# Calculate deviation
+print("Deviation: " + str(np.std(results)))
--- touchlist.py
+++ touchlist.py
@@ -2,7 +2,7 @@
 import numpy as np
 import random
 
-for i in range(250):
+for i in range(100):
     name = ""
     for i in range(12):
         new = random.choice(["A","B","C","D","E",'F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y'])
