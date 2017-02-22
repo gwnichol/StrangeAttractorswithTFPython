3df579393ceec5a069437751ec3f9635aaf048a2
Grant Nichol <gwnichol.m@gmail.com>
2017-02-22 00:37:12 +0000

    Add ability to set return number

--- touchlist.py
+++ touchlist.py
@@ -1,8 +1,13 @@
 # Touch List
 import numpy as np
 import random
+import sys
 
-for i in range(100):
+if len(sys.argv) == 2:
+    size = int(sys.argv[1])
+else:
+    size = 250
+for i in range(size):
     name = ""
     for i in range(12):
         new = random.choice(["A","B","C","D","E",'F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y'])
